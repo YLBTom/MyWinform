7b7983665356e6448c31c1e20153cddd285473f6
YLBTom <robby021@126.com>
2019-01-26 18:57:48 +0800

    first

--- NewForm/Form1.Designer.cs
+++ NewForm/Form1.Designer.cs
@@ -29,22 +29,35 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(66, 48);
+            this.textBox1.Location = new System.Drawing.Point(91, 64);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(104, 25);
+            this.textBox1.Size = new System.Drawing.Size(79, 21);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "一个文本";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "按钮";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(642, 383);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
